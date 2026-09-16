f4e1749f0f4b9f88f24efe6bdb557a3d7b71d45b
Juan Pablo Leon Fonseca <fonseca123leon@gmail.com>
2026-09-16 14:30:55 -0500

    ci: agregar wokflows despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-  base: "/frontend-supermarket-inventory/"
+  base: '/frontend-supermarket-inventory/',
 })
