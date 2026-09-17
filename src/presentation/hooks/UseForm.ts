d21481b394099293025ffcf3b3ee217f28c91212
Juan Pablo Leon Fonseca <fonseca123leon@gmail.com>
2026-09-17 16:50:38 -0500

    haciendo rsponsive para dispositivos mobiles el login

--- src/presentation/hooks/UseForm.ts
+++ src/presentation/hooks/UseForm.ts
@@ -1,24 +1,23 @@
-import { useState, type ChangeEvent, type SubmitEvent } from "react";
+import { useState, type ChangeEvent } from "react";
 
 export function useFormulary<T>(initialValues: T) {
+  const [values, setValues] = useState<T>(initialValues);
 
-    const [values, setValues] = useState<T>(initialValues);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    setValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-        const { name, value } = event.target;
-        setValues((prev) => ({
-        ...prev,
-        [name]: value,
-        }));
-    }
+  const resetForm = (): void => {
+    setValues(initialValues);
+  };
 
-    const resetForm = (): void => {
-        setValues(initialValues);
-    };
-
-    return  {
-        handleChange,
-        resetForm,
-        values
-    }
+  return {
+    handleChange,
+    resetForm,
+    values,
+  };
 }
